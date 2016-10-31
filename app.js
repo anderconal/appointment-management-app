9dbf5d67d49ccd548f1339a9c477f7f92c2e442e
Ander Conal <ander.conal@jinnapp.com>
2016-10-31 13:52:05 +0100

    Local authentication working

--- app.js
+++ app.js
@@ -1,38 +1,65 @@
+/*
+  Setup
+  Get all the tools we need.
+*/
 var express = require('express');
+var app = express();
+// Utilities for working with file and directory paths.
 var path = require('path');
+// Node.js middleware for serving a favicon.
 var favicon = require('serve-favicon');
-var logger = require('morgan');
+// HTTP request logger middleware for node.js.
+var morgan = require('morgan');
+/*
+  Parse Cookie header and populate req.cookies with an object keyed by the
+  cookie names.
+*/
 var cookieParser = require('cookie-parser');
+/*
+  Parse incoming request bodies in a middleware before your handlers,
+  available under the req.body property.
+*/
 var bodyParser = require('body-parser');
+// Simple, unobtrusive authentication for Node.js.
+var passport = require('passport');
+/*
+  The flash is a special area of the session used for storing messages.
+  Messages are written to the flash and cleared after being displayed to the
+  user. The flash is typically used in combination with redirects, ensuring
+  that the message is available to the next page that is to be rendered.
+*/
+var flash = require('connect-flash');
+// Simple session middleware for Express.
+var session = require('express-session');
 
 /*
-  Mongoose
-  We have to initialize all the Mongoose configuration before require the
-  routes.
+  DB
 */
-
 var mongoose = require('mongoose');
-require('./models/Customers');
-require('./models/Events');
+var configDB = require('./config/database.js');
 
-mongoose.connect('mongodb://localhost/app-management-app');
+// Models
+require('./models/Customers.js'); // Customer Model.
+require('./models/Events.js'); // Event Model.
+require('./models/Users.js'); // User Model.
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+/*
+  Routes
+*/
+var routes = require('./routes/index.js');
+var users = require('./routes/users.js');
 
-var app = express();
+/*
+  Configuration
+*/
+mongoose.connect(configDB.url); // Connect with our MongoDB.
+require('./config/passport.js')(passport); // Our Passport main configuration.
 
-// view engine setup
+/*
+  View engine setup
+*/
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 /*
   Any Bower components are loaded from HTML like this:
@@ -46,20 +73,41 @@ app.use('/bower_components',  express.static(__dirname + '/bower_components'));
 */
 app.use('/node_modules',  express.static(__dirname + '/node_modules'));
 
+/*
+  Required for passport
+*/
+app.use(session({
+  secret: 'anderconalappointment-management-app',
+  resave: false,
+  saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session()); // Persistent login sessions.
+app.use(flash()); // Use connect-flash for flash messages stored in session.
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.use('/', routes);
 app.use('/users', users);
 
-// catch 404 and forward to error handler
+// Catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
-// error handlers
+/*
+  Error handlers
+*/
 
-// development error handler
-// will print stacktrace
+// Development error handler, will print stack trace
 if (app.get('env') === 'development') {
   app.use(function(err, req, res, next) {
     res.status(err.status || 500);
@@ -70,8 +118,7 @@ if (app.get('env') === 'development') {
   });
 }
 
-// production error handler
-// no stacktraces leaked to user
+// Production error handler, no stack traces leaked to user
 app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error', {
@@ -79,6 +126,5 @@ app.use(function(err, req, res, next) {
     error: {}
   });
 });
-
 
 module.exports = app;
---
+++ config/passport.js
@@ -0,0 +1,119 @@
+/*
+  Load all the things we need
+*/
+var LocalStrategy   = require('passport-local').Strategy;
+var mongoose = require('mongoose');
+var User = mongoose.model('User');
+
+/*
+  Expose this function to our app using module.exports.
+*/
+module.exports = function(passport) {
+
+/**********/
+// Passport session setup
+/**********/
+/*
+  Required for persistent login sessions, passport needs ability to serialize
+  and unserialize users out of session.
+*/
+
+// Used to serialize the user for the session.
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+// Used to deserialize the user.
+passport.deserializeUser(function(id, done) {
+  User.findById(id, function(err, user) {
+    done(err, user);
+  });
+});
+
+/**********/
+// Local signup
+/**********/
+/*
+  We are using named strategies since we have one for login and one for signup
+  by default, if there was no name, it would just be called 'local'.
+*/
+
+passport.use('local-signup', new LocalStrategy({
+  usernameField : 'username',
+  passwordField : 'password',
+  // Allows us to pass back the entire request to the callback.
+  passReqToCallback : true
+}, function(req, username, password, done) {
+    // Asynchronous, User.findOne wont fire unless data is sent back.
+    process.nextTick(function() {
+      /*
+        Find a user whose username is the same as the forms username
+        we are checking to see if the user trying to login already exists.
+      */
+      User.findOne({ 'local.username' :  username }, function(err, user) {
+        // If there are any errors, return the error.
+        if (err)
+          return done(err);
+
+          // Check to see if theres already a user with that username.
+          if (user) {
+            return done(null, false, req.flash('signupMessage',
+                                      'That username is already taken.'));
+          } else {
+              // If there is no user with that username, create the user.
+              var newUser = new User();
+
+              // Set the user's local credentials.
+              newUser.local.username    = username;
+              newUser.local.password = newUser.generateHash(password);
+
+              // Save the user.
+              newUser.save(function(err) {
+                if (err)
+                  throw err;
+                  return done(null, newUser);
+              });
+          } // End of check to see if theres already a user with that username.
+      }); // End of the findOne function's anonymous function.
+    }); // End of the nextTick function.
+  })); // End of the passport.use function's anonymous function.
+
+/**********/
+// Local login
+/**********/
+/*
+  We are using named strategies since we have one for login and one for signup
+  by default, if there was no name, it would just be called 'local'
+*/
+passport.use('local-login', new LocalStrategy({
+  usernameField : 'username',
+  passwordField : 'password',
+  // Allows us to pass back the entire request to the callback
+  passReqToCallback : true
+  // Callback with username and password from our form
+}, function(req, username, password, done) {
+    /*
+      Find a user whose username is the same as the forms username we are
+      checking to see if the user trying to login already exists
+    */
+    User.findOne({ 'local.username' :  username }, function(err, user) {
+      // If there are any errors, return the error before anything else
+      if (err)
+        return done(err);
+
+      // If no user is found, return the message
+      if (!user)
+        // req.flash is the way to set flashdata using connect-flash
+        return done(null, false, req.flash('loginMessage', 'No user found.'));
+
+      // If the user is found but the password is wrong
+      if (!user.validPassword(password))
+        // Create the loginMessage and save it to session as flashdata
+        return done(null, false, req.flash('loginMessage',
+                                  'Oops! Wrong password.'));
+
+      // All is well, return successful user
+        return done(null, user);
+    }); // End of the findOne function.
+  })); // End of the passport.use function's anonymous function.
+}; // End of the module.exports anonymous function.
